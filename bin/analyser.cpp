a125cd54e3e9a4105b1327392dc6aa12a3d62ff0
Kinga Anna Wozniak <kingusiu@gmail.com>
2019-07-05 16:42:06 +0200

    adding tracking of bin/ dir with analyzer executable

---
+++ bin/analyser.cpp
@@ -0,0 +1,30 @@
+/*
+ * analyser_EXEC.cpp
+ *
+ *  Created on: 24 Aug 2016
+ *      Author: jkiesele
+ */
+
+
+#include <iostream>
+#include "interface/analyser.h"
+
+int main(int argc, char* argv[]){
+
+	if(argc!=2){
+		std::cout << "analyser: need exactly one input file" <<std::endl;
+		exit (-1);
+	}
+
+
+	std::string inputfile=argv[1];
+
+	analyser analyser;
+
+
+	analyser.readConfigFile(inputfile);
+
+	analyser.start();
+
+	return 1;
+}
